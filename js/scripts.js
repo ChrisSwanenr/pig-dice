1802020f0f9183cab3e2f2d7d828129150e43948
Chris Swanner and Joel Adams <joel@>
2018-01-24 11:00:31 -0800

    add result panels and UI tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -16,8 +16,12 @@ $("#roller1").click(function() {
   if (result === 1) {
     player1TurnScore.length = 0;
     player1Result.push(0);
+    $("#roller1").hide();
+    $("#hold1").hide();
+    $("#roller2").show();
   } else {
     player1TurnScore.push(result);
+    $("#hold1").show();
   }
   alert(player1TurnScore);
 });
@@ -27,13 +31,20 @@ $("#roller2").click(function() {
   if (result === 1) {
     player2TurnScore.length = 0;
     player2Result.push(0);
+    $("#roller2").hide();
+    $("#hold2").hide();
+    $("#roller1").show();
   } else {
     player2TurnScore.push(result);
+    $("#hold2").show();
   }
   alert(player2TurnScore);
 });
 
 $("#hold1").click(function() {
+    $("#hold1").hide()
+    $("#roller1").hide()
+    $("#roller2").show()
     var turnScore = player1TurnScore.reduce(function (x, y) {
     return x + y;
     });
@@ -41,12 +52,14 @@ $("#hold1").click(function() {
     var player1Score = player1Result.reduce(function (x,y) {
         return x + y;
       });
-      player1TurnScore.length = 0;
-      $(".p1Score").text(player1Score);
-
+    player1TurnScore.length = 0;
+    $(".p1Score").text(player1Score);
 });
 
 $("#hold2").click(function() {
+    $("#hold2").hide()
+    $("#roller2").hide()
+    $("#roller1").show()
     var turnScore = player2TurnScore.reduce(function (x, y) {
     return x + y;
     });
@@ -54,10 +67,9 @@ $("#hold2").click(function() {
     var player2Score = player2Result.reduce(function (x,y) {
         return x + y;
       });
-      player2TurnScore.length = 0;
-      $(".p2Score").text(player2Score);
-
-})
+    player2TurnScore.length = 0;
+    $(".p2Score").text(player2Score);
+});
 
 
 
